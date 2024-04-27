288b6d6565ef4e34a433c0c3c78e74241d773d9a
Speedrun <86620608+Bayu12345677@users.noreply.github.com>
2024-04-27 16:38:25 +0700

    Update config.lua

--- Ketupat-terror/config.lua
+++ Ketupat-terror/config.lua
@@ -1,6 +1,6 @@
 ## server ⚈
-status_apk="maintance"
-versi_new="1.0.1"
+status_apk="online"
+versi_new="1.0.2"
 versi_old="1.0.0"
 ## Config </>
-SC_tk="https://sfl.gl/DyXwn"
+SC_tk="https://moneyblink.com/0ESftuRC41ec"
